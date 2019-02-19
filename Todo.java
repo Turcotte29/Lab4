b3b845dbe798ab8fcc42da47f76d54b4ae40b87f
Ryan Turcotte <ryanturcotte@ou.edu>
2019-02-19 14:04:09 -0600

    This is everything from lab 4

---
+++ Todo.java
@@ -0,0 +1,42 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-04-07
+OU ID_______________: 113396927
+OU EMAIL____________: ryanturcotte@ou.edu
+
+Tasks:
+1. Finish equals method in Animal class
+2. Test Animal full constructor,
+3. Test Animal getters 
+4. Test Anima; toString
+5. Finish equals method in Equipment class
+6. Test equipment constructor 
+7. Test equipment toString
+8. Test zoo getTotalHeight
+9. Test zoo getCapacity
+10. Test Zoo toString
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 15
+6. 12
+7. 15
+8. 7
+9. 5
+10.10
+
+Actual Time (minutes):
+1. 5
+2. 20
+3. 20
+4. 30
+5. 15
+6. 10
+7. 15
+8. 20
+9. 15
+10. 15
+*/}
